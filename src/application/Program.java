f7fc992402e4a5e40d73c42da0c52ab15ab7f267
EnricoTaboga <enrico.taboga@icarotech.com>
2021-01-22 10:46:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,11 @@ public class Program {
 		
 		Department depart = new Department(1, "Books");
 		
+		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.00, depart);
+		
 		System.out.println(depart);
+		
+		System.out.println(seller);
 
 	}
 
